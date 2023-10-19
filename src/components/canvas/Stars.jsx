c699b10e02a02bf11da40b14b10bf2ddcbc8dfce
Bryan CHAVARRIA <b.chavarria@lacroix.group>
2023-10-19 13:34:01 -0600

    In process contact form

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState, useRef, Suspense } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial, Preload } from '@react-three/drei';
+import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = () => {
+  const ref = useRef();
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 })
+
   return (
-    <div>Stars</div>
+    <group>
+      <points ref={ref} positions={sphere} stride={3} frustumCulled />
+    </group>
   )
 }
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className='w-full h-full absolute inset-0 z-[-1]'>
+      <Canvas camera={{ position: [0, 0, 1]}}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+      
+      <Preload all />
+      </Canvas>
+    </div>
+  )
+}
+
+export default StarsCanvas;
