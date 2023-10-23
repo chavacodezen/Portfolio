7b9faad5590946ffeb17fa35e4ad865b207411da
Bryan CHAVARRIA <b.chavarria@lacroix.group>
2023-10-23 14:35:34 -0600

    going back to old background

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "black",
+        primary: "#050816",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
